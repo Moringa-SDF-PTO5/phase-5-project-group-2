ef89cc73ad219efa75123ceabfdac726992ff5d7
marynjoroge <wangarinjorogekenya@gmail.com>
2024-07-24 14:00:09 +0300

    create database

--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost/ecommerce_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:6789@localhost/ecommerce_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
